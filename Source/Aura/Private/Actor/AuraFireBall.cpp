2de49c6fb110dc3bd481ec42ef3cd2473070f55f
hy396 <144646807+hy396@users.noreply.github.com>
2026-07-16 23:54:22 +0800

    feat(actor): AuraFireBall 火球 Actor
    - 火球投射物，含爆炸 AOE 与 Niagara 特效

---
+++ Source/Aura/Private/Actor/AuraFireBall.cpp
@@ -0,0 +1,72 @@
+﻿// Huanyu,miaomiaomiao,nekoneko
+
+
+#include "Actor/AuraFireBall.h"
+
+#include "AbilitySystemBlueprintLibrary.h"
+#include "MyGameplayTags.h"
+#include "GameplayCueManager.h"
+#include "AbilitySystem/AuraAbilitySystemLibrary.h"
+#include "Components/AudioComponent.h"
+
+// Called when the game starts or when spawned
+void AAuraFireBall::BeginPlay()
+{
+	Super::BeginPlay();
+	
+	StartOutgoingTimeline(); //调用开始时间线修改
+}
+
+void AAuraFireBall::OnSphereOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
+	UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	// 检查重叠有效性，若无效则提前退出
+	if (!IsValidOverlap(OtherActor)) return;
+	
+	// 仅在服务器端执行伤害逻辑
+	if (HasAuthority())
+	{
+		// 获取目标Actor的Ability System组件
+		if (UAbilitySystemComponent* TargetASC = UAbilitySystemBlueprintLibrary::GetAbilitySystemComponent(OtherActor))
+		{
+			// 受击力设置
+			const FVector KnockbackDirection = GetActorForwardVector().RotateAngleAxis(-45.f, GetActorRightVector());
+			DamageEffectParams.KnockbackForce = KnockbackDirection * DamageEffectParams.KnockbackForceMagnitude;
+
+			// 根据火球前进方向计算死亡冲击力
+			const FVector DeathImpulse = GetActorForwardVector() * DamageEffectParams.DeathImpulseMagnitude;
+			DamageEffectParams.DeathImpulse = DeathImpulse;
+			
+			// 设置目标Ability System组件并应用伤害效果
+			DamageEffectParams.TargetAbilitySystemComponent = TargetASC;
+			UAuraAbilitySystemLibrary::ApplyDamageEffect(DamageEffectParams);
+		}
+	}
+}
+
+void AAuraFireBall::OnHit()
+{
+	// 若拥有者存在，触发火球爆炸的Gameplay效果
+	if (GetOwner())
+	{
+		FGameplayCueParameters CueParams;
+		CueParams.Location = GetActorLocation(); // 设置效果触发位置为火球当前位置
+		UGameplayCueManager::ExecuteGameplayCue_NonReplicated( // 非网络同步触发Gameplay cue
+			GetOwner(), 
+			FMyGameplayTags::Get().GameplayCue_FireBlast, // 使用预定义的"火球爆炸"效果标签
+			CueParams
+		);
+	}
+
+	// 停止并销毁持续播放的声音组件
+	if (LoopingSoundComponent)
+	{
+		LoopingSoundComponent->Stop();
+		LoopingSoundComponent->DestroyComponent();
+	}
+
+	// 标记火球已命中目标
+	bHit = true;
+}
+
+
---
+++ Source/Aura/Public/Actor/AuraFireBall.h
@@ -0,0 +1,44 @@
+﻿// Huanyu,miaomiaomiao,nekoneko
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AuraProjectile.h"
+#include "AuraFireBall.generated.h"
+
+/**
+ * 火焰爆发使用的火球类
+ */
+UCLASS()
+class AURA_API AAuraFireBall : public AAuraProjectile
+{
+	GENERATED_BODY()
+
+public:
+	// 执行蓝图时间轴事件，需要在蓝图中实现此事件
+	UFUNCTION(BlueprintImplementableEvent)
+	void StartOutgoingTimeline();
+	
+	// 当前火球返回的目标角色，默认是技能的释放者，在创建火球是创建
+	UPROPERTY(BlueprintReadOnly)
+	TObjectPtr<AActor> ReturnToActor;
+	// 火球爆炸造成的伤害
+	UPROPERTY(BlueprintReadWrite)
+	FDamageEffectParams ExplosionDamageParams;
+	
+protected:
+	virtual void BeginPlay() override;
+	
+	/**
+	 * 发射物发生碰撞后的回调函数
+	 * @param OverlappedComponent 发生重叠事件的自身的碰撞体对象
+	 * @param OtherActor 目标的actor对象
+	 * @param OtherComp 目标的碰撞体组件
+	 * @param OtherBodyIndex 目标身体的索引
+	 * @param bFromSweep 是否为瞬移检测到的碰撞
+	 * @param SweepResult 如果位置发生过瞬移（直接设置到某处），两个位置中间的内容会记录到此对象内
+	 */
+	virtual void OnSphereOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult) override;
+
+	virtual void OnHit() override;
+};
