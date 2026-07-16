4aabce825a0d04f88eea20820812d6f5430ee601
hy396 <144646807+hy396@users.noreply.github.com>
2026-07-16 23:48:52 +0800

    feat(ability): AuraGameplayAbility 主动技能基类
    - 所有法术派生基类，提供激活时蓝图钩子

---
+++ Source/Aura/Private/AbilitySystem/Abilities/AuraGameplayAbility.cpp
@@ -0,0 +1,59 @@
+// Huanyu,miaomiaomiao,nekoneko
+
+
+#include "AbilitySystem/Abilities/AuraGameplayAbility.h"
+
+#include "AbilitySystem/AuraAttributeSet.h"
+
+FString UAuraGameplayAbility::GetDescription(int32 Level)
+{
+	return FString::Printf(TEXT("%s, <Level>%d</>"), L"默认技能名称 - 请覆写技能的GetDescription来实现技能的描述", Level);
+}
+
+FString UAuraGameplayAbility::GetNextLevelDescription(int32 Level)
+{
+	return FString::Printf(TEXT("下一等级：<Level>%d</> 请覆写技能的GetNextLevelDescription来实现技能的描述。"), Level);
+}
+
+FString UAuraGameplayAbility::GetDescriptionAtLevel(int32 Level, const FString& Title)
+{
+	return FString::Printf(TEXT("通用的技能描述：<Level>%d</> 在GetDescription和GetNextLevelDescription抽离出的公用的部分实现的函数。"), Level);
+}
+
+FString UAuraGameplayAbility::GetLockedDescription(int32 Level)
+{
+	return FString::Printf(TEXT("技能将在角色等级达到<Level>%d</>时解锁"), Level);
+}
+
+float UAuraGameplayAbility::GetManaCost(float InLevel) const
+{
+	float ManaCost = 0.f;
+	//获取到冷却GE
+	if (const UGameplayEffect* CostEffect = GetCostGameplayEffect())
+	{
+		//遍历GE修改的内容
+		for (FGameplayModifierInfo Mod : CostEffect->Modifiers)
+		{
+			//判断修改的属性是否为角色蓝量属性
+			if (Mod.Attribute == UAuraAttributeSet::GetManaAttribute())
+			{
+				//通过修饰符获取到使用的FScalableFloat并计算传入等级的蓝量消耗，FScalableFloat是受保护性的属性，无法直接获取，只能通过函数
+				Mod.ModifierMagnitude.GetStaticMagnitudeIfPossible(InLevel, ManaCost);
+				break;//获取到了就结束遍历
+			}
+		}
+	}
+	return FMath::Abs(ManaCost);
+}
+
+float UAuraGameplayAbility::GetCooldown(float InLevel) const
+{
+	float Cooldown = 0.f;
+	//获取到技能冷却GE
+	if (const UGameplayEffect* CooldownEffect = GetCooldownGameplayEffect())
+	{
+		//获取到当前冷却时间
+		CooldownEffect->DurationMagnitude.GetStaticMagnitudeIfPossible(InLevel, Cooldown);
+	}
+	return Cooldown;
+}
---
+++ Source/Aura/Public/AbilitySystem/Abilities/AuraGameplayAbility.h
@@ -0,0 +1,50 @@
+// Huanyu,miaomiaomiao,nekoneko
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Abilities/GameplayAbility.h"
+#include "AuraGameplayAbility.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AURA_API UAuraGameplayAbility : public UGameplayAbility
+{
+	GENERATED_BODY()
+public:
+
+	UPROPERTY(EditDefaultsOnly, Category="Input")
+	FGameplayTag StartupInputTag;
+
+	/**
+	 * 获取当前等级技能描述
+	 * @param Level 技能等级
+	 * @return 技能描述富文本
+	 */
+	virtual FString GetDescription(int32 Level);
+
+	/**
+	 * 获取下一等级技能描述
+	 * @param Level 下一等级
+	 * @return 技能描述附文本
+	 */
+	virtual FString GetNextLevelDescription(int32 Level);
+	/**
+	 * 当前技能的技能描述，将共用部分抽离出来为单个函数
+	 * @param Level 显示的技能等级
+	 * @param Title 技能标题
+	 * @return 技能描述文本
+	 */
+	virtual FString GetDescriptionAtLevel(int32 Level, const FString& Title);
+
+	// 获取锁定技能描述
+	static FString GetLockedDescription(int32 Level);
+
+protected:
+
+	float GetManaCost(float InLevel = 1.f) const;
+	float GetCooldown(float InLevel = 1.f) const;
+
+};
