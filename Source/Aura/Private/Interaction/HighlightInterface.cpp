9489e3b970ab77f400675324a7eb06a9410e158a
hy396 <144646807+hy396@users.noreply.github.com>
2026-07-17 00:23:28 +0800

    feat(interface): HighlightInterface 高亮接口
    - 鼠标悬停高亮接口（Outline 后处理）

---
+++ Source/Aura/Private/Interaction/HighlightInterface.cpp
@@ -0,0 +1,7 @@
+﻿// Huanyu,miaomiaomiao,nekoneko
+
+
+#include "Interaction/HighlightInterface.h"
+
+
+// Add default functionality here for any IHighlightInterface functions that are not pure virtual.
---
+++ Source/Aura/Public/Interaction/HighlightInterface.h
@@ -0,0 +1,40 @@
+﻿// Huanyu,miaomiaomiao,nekoneko
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Interface.h"
+#include "HighlightInterface.generated.h"
+
+// This class does not need to be modified.
+UINTERFACE(MinimalAPI, BlueprintType)
+class UHighlightInterface : public UInterface
+{
+	GENERATED_BODY()
+};
+
+/**
+ * 
+ */
+class AURA_API IHighlightInterface
+{
+	GENERATED_BODY()
+
+	// Add interface functions to this class. This is the class that will be inherited to implement this interface.
+public:
+
+	//高亮 描边
+	UFUNCTION(BlueprintNativeEvent)
+	void HighlightActor();
+	
+	//取消高亮 描边
+	UFUNCTION(BlueprintNativeEvent)
+	void UnHighlightActor();
+
+	/**
+	 * 设置角色移动的目标位置
+	 * @param OutDestination 引用，调用将目标位置设置到此值
+	 */
+	UFUNCTION(BlueprintNativeEvent)
+	void SetMoveToLocation(FVector& OutDestination);
+};
