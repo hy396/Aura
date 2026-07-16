658cea6d82b1cab2960f45d354f25e3dad5cf155
hy396 <144646807+hy396@users.noreply.github.com>
2026-07-17 00:23:37 +0800

    feat(interface): SaveInterface 存档接口
    - 存档接口（LoadActor / SaveActor）

---
+++ Source/Aura/Public/Interaction/SaveInterface.h
@@ -0,0 +1,33 @@
+// Huanyu,miaomiaomiao,nekoneko
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Interface.h"
+#include "SaveInterface.generated.h"
+
+// This class does not need to be modified.
+UINTERFACE(MinimalAPI)
+class USaveInterface : public UInterface
+{
+	GENERATED_BODY()
+};
+
+/**
+ * 
+ */
+class AURA_API ISaveInterface
+{
+	GENERATED_BODY()
+
+	// Add interface functions to this class. This is the class that will be inherited to implement this interface.
+public:
+	
+	//设置Actor是否需要修改位置变换
+	UFUNCTION(BlueprintCallable, BlueprintNativeEvent)
+	bool ShouldLoadTransform();
+
+	//在存档读取完数据后，调用更新Actor
+	UFUNCTION(BlueprintCallable, BlueprintNativeEvent)
+	void LoadActor();
+};
