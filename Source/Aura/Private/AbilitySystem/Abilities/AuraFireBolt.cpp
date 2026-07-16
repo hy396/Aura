360aa5eaa6832bfa336c7bba68b82b46aa64ebae
hy396 <144646807+hy396@users.noreply.github.com>
2026-07-16 23:49:08 +0800

    feat(spell): AuraFireBolt 火球投射法术
    - 单体投射的火球技能，带弹道追踪

---
+++ Source/Aura/Private/AbilitySystem/Abilities/AuraFireBolt.cpp
@@ -0,0 +1,110 @@
+// Huanyu,miaomiaomiao,nekoneko
+
+
+#include "AbilitySystem/Abilities/AuraFireBolt.h"
+
+#include "AbilitySystem/AuraAbilitySystemLibrary.h"
+#include "Aura/Public/MyGameplayTags.h"
+
+
+FString UAuraFireBolt::GetDescription(int32 Level)
+{
+	return GetDescriptionAtLevel(Level, L"火球术");
+}
+
+FString UAuraFireBolt::GetNextLevelDescription(int32 Level)
+{
+	return GetDescriptionAtLevel(Level, L"下一等级");
+}
+
+FString UAuraFireBolt::GetDescriptionAtLevel(int32 Level, const FString& Title)
+{
+	// 伤害
+	const int32 ScaledDamage = Damage.GetValueAtLevel(Level);
+	// 法力消耗
+	const float ManaCost = GetManaCost(Level);
+	// 技能冷却
+	const float Cooldown = GetCooldown(Level);
+	
+	return FString::Printf(TEXT(
+		// 标题
+		"<Title>%s</>\n"
+
+		// 细节
+		"<Small>Level:</> <Level>%i</>\n"
+		"<Small>技能冷却：</> <Cooldown>%.1f</>\n"
+		"<Small>蓝量消耗：</> <ManaCost>%.1f</>\n\n"//%.1f会四舍五入到小数点后一位
+
+		// 技能描述
+		"<Default>发射 %i 颗火球，在发生撞击时产生爆炸，并造成</> <Damage>%i</> <Default>点火焰伤害，并有一定几率燃烧。</>"),
+
+		// 动态修改值
+		*Title,
+		Level,
+		Cooldown,
+		ManaCost,
+		FMath::Min(Level, NumProjectiles),
+		ScaledDamage);
+}
+
+void UAuraFireBolt::SpawnProjectiles(const FVector& ProjectileTargetLocation, const FGameplayTag& SocketTag,
+	 const bool bOverridePitch, const float PitchOverride, AActor* HomingTarget)
+{
+	// 判断此函数是否在服务器运行
+	if (!GetAvatarActorFromActorInfo()->HasAuthority()) return;
+	// 获取释放位置
+	const FVector SocketLocation = ICombatInterface::Execute_GetCombatSocketLocation(
+		GetAvatarActorFromActorInfo(),
+		SocketTag);
+	// 将方向转为旋转
+	FRotator Rotation = (ProjectileTargetLocation - SocketLocation).Rotation();
+	// 覆写发射角度
+	if (bOverridePitch) Rotation.Pitch = PitchOverride;
+	// 获取朝向向量
+	const FVector Forward = Rotation.Vector();
+	// 限制产生火球的最大数量
+	const int32 EffectiveNumProjectiles = FMath::Min(MaxNumProjectiles, GetAbilityLevel());
+	// 根据函数获取到所有生成的转向
+	TArray<FRotator> Rotations = UAuraAbilitySystemLibrary::EvenlySpacedRotators(Forward, FVector::UpVector, ProjectileSpread, EffectiveNumProjectiles);
+	//遍历所有朝向，并生成火球术
+	for(FRotator& Rot : Rotations)
+	{
+		FTransform SpawnTransform;
+		SpawnTransform.SetLocation(SocketLocation);
+		SpawnTransform.SetRotation(Rot.Quaternion());
+				
+		//SpawnActorDeferred将异步创建实例，在实例创建完成时，相应的数据已经应用到了实例身上
+		AAuraProjectile* Projectile = GetWorld()->SpawnActorDeferred<AAuraProjectile>(
+			ProjectileClass,
+			SpawnTransform,
+			GetOwningActorFromActorInfo(),
+			CurrentActorInfo->PlayerController->GetPawn(),
+			ESpawnActorCollisionHandlingMethod::AlwaysSpawn);
+
+		Projectile->DamageEffectParams = MakeDamageEffectParamsFromClassDefaults();
+
+		//根据目标类型设置HomingTargetComponent，此内容必须在飞弹被生成后设置
+		if(HomingTarget && HomingTarget->Implements<UCombatInterface>())
+		{
+			//设置攻击的位置为攻击对象的根位置
+			Projectile->ProjectileMovement->HomingTargetComponent = HomingTarget->GetRootComponent();
+		}
+		else
+		{
+			//如果没有获取到攻击目标，则创建一个可销毁的并应用
+			Projectile->HomingTargetSceneComponent = NewObject<USceneComponent>(USceneComponent::StaticClass());
+			Projectile->HomingTargetSceneComponent->SetWorldLocation(ProjectileTargetLocation); //设置组件位置
+			Projectile->ProjectileMovement->HomingTargetComponent = Projectile->HomingTargetSceneComponent;
+		}
+		//设置飞弹朝向目标时的加速度
+		Projectile->ProjectileMovement->HomingAccelerationMagnitude = FMath::FRandRange(HomingAccelerationMin, HomingAccelerationMax);
+		Projectile->ProjectileMovement->bIsHomingProjectile = bLaunchHomingProjectiles; //设置为true，飞弹将加速飞向攻击目标
+
+		//确保变换设置被正确应用
+		Projectile->FinishSpawning(SpawnTransform);
+
+		//Debug
+		//UKismetSystemLibrary::DrawDebugArrow(GetAvatarActorFromActorInfo(), SocketLocation, SocketLocation + Rot.Vector() * 100.f, 5, FLinearColor::Green, 120, 5);
+	}
+	
+}
---
+++ Source/Aura/Public/AbilitySystem/Abilities/AuraFireBolt.h
@@ -0,0 +1,73 @@
+// Huanyu,miaomiaomiao,nekoneko
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AbilitySystem/Abilities/ProjectileSpell.h"
+#include "AuraFireBolt.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AURA_API UAuraFireBolt : public UProjectileSpell
+{
+	GENERATED_BODY()
+public:
+	/**
+	 * 获取当前等级技能描述
+	 * @param Level 技能等级
+	 * @return 技能描述富文本
+	 */
+	virtual FString GetDescription(int32 Level) override;
+	
+	/**
+	 * 获取下一等级技能描述
+	 * @param Level 下一等级
+	 * @return 技能描述附文本
+	 */
+	virtual FString GetNextLevelDescription(int32 Level) override;
+
+	/**
+	 * 当前技能的技能描述，将共用部分抽离出来为单个函数
+	 * @param Level 显示的技能等级
+	 * @param Title 技能标题
+	 * @return 技能描述文本
+	 */
+	virtual FString GetDescriptionAtLevel(int32 Level, const FString& Title) override; //获取对应等级的技能描述
+
+	/**
+	 * 生成多个火球函数，需要设置使用的骨骼
+	 * 
+	 * @param ProjectileTargetLocation 目标位置
+	 * @param SocketTag 骨骼标签，用于判断骨骼位置是属于身体还是武器
+	 * @param bOverridePitch 覆盖当前生成火球的垂直角度
+	 * @param PitchOverride 覆盖的值
+	 * @param HomingTarget 攻击的目标
+	 *
+	 */	
+	UFUNCTION(BlueprintCallable, Category="Projectile")
+	void SpawnProjectiles(const FVector& ProjectileTargetLocation, const FGameplayTag& SocketTag, const bool bOverridePitch = false, const float PitchOverride = 0.f, AActor* HomingTarget = nullptr);
+
+protected:
+	// 技能攻击范围角度
+	UPROPERTY(EditDefaultsOnly, Category="FireBolt")
+	float ProjectileSpread = 90.f;
+
+	// 技能攻击范围角度
+	UPROPERTY(EditDefaultsOnly, Category="FireBolt")
+	int32 MaxNumProjectiles = 5;
+
+	// 移动朝向目标的最小加速度
+	UPROPERTY(EditDefaultsOnly, Category="FireBolt")
+	float HomingAccelerationMin = 1600.f;
+
+	// 移动朝向目标的最大加速度
+	UPROPERTY(EditDefaultsOnly, Category="FireBolt")
+	float HomingAccelerationMax = 3200.f;
+
+	// 设置生成的飞弹是否需要朝向目标飞行
+	UPROPERTY(EditDefaultsOnly, Category="FireBolt")
+	bool bLaunchHomingProjectiles = true;
+
+};
